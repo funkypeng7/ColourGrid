c19fb0269a3dd9e92eddc713320e1cec7bbb8f03
hbdnz <hbndnz@gmail.com>
2020-10-27 11:28:43 +1300

    Harry update

--- Source/rushhour.cpp
+++ Source/rushhour.cpp
@@ -24,8 +24,11 @@
 // Yellow = 9
 // Black = 10
 // Aqua = 11
+// Turquoise = 12 (0, 153, 153)
 
-std::vector<std::vector<int>> EASY1 {{1, 0, 2, 4, 2, 4, 3}, {2, 1, 2, 1, 2, 1, 3}};
+std::vector<std::vector<int>> EASY1 {{1, 1, 2, 4, 2, 4, 3}, {2, 1, 2, 1, 2, 1, 3}, {3, 1, 3, 3, 2, 3, 4}, {4, 0, 2, 6, 2, 5, 2}, {5, 0, 2, 5, 5, 4, 5}, {6, 0, 2, 2, 4, 1, 4}, {7, 0, 3, 5, 7, 3, 7}, {8, 1, 2, 2, 6, 2, 7}, {9, 1, 3, 6, 5, 6, 7}, {10, 1, 2, 1, 5, 1, 6}, {11, 0, 2, 4, 6, 3, 6}};
+std::vector<std::vector<int>> EASY2 {{1, 1, 2, 4, 3, 4, 4}, {4, 1, 2, 3, 3, 3, 4}, {5, 0, 2, 2, 3, 1, 3}, {7, 0, 3, 3, 7, 1, 7}, {9, 0, 3, 4, 5, 2, 5}, {11, 1, 2, 1, 4, 1, }};
+std::vector<std::vector<int>> EASY3 {{1, 1, 2, 4, 3, 4, 4}, {3, 1, 3, 3, 5, 3, 6}, {4, 0, 2, 3, 4, 2, 4}, {5, 0, 2, 2, 7, 1, 7}, {7, 0, 3, 6, 5, 4, 5}, {8, 1, 2, 2, 6, 2, 7}, {9, 1, 3, 6, 5, 6, 7}, {10, 1, 2, 1, 5, 1, 6}, {11, 0, 2, 4, 6, 3, 6}};
 
 void RushhourInit()
 {
@@ -34,7 +37,7 @@ void RushhourInit()
     for(i = 1; i < 8; i++) {
         gridInfo[0][i] = 'SIDE';
     }
-
+ 
     for(i = 1; i < 8; i++) {
         gridInfo[9][i] = 'SIDE';
     }
@@ -43,18 +46,46 @@ void RushhourInit()
     gridInfo[9][0] = 'CLICKRIGHT';
 
     for(i = 1; i < 9; i++) {
-        for(j = 0; j < 7; j++) {
-            gridInfo[i][j] = 'SPACE';
-        }
-    }
-
-    for(i = 1; i < 9; i++) {
         for(j= 0; j < 7; j++) {
             gridInfo[i][j] = 'WALL';
         }
     }
 
-    gridInfo[8][3] = 'EXIT';
+    for(i = 2; i < 8; i++) {
+        for(j = 1; j < 6; j++) {
+            gridInfo[i][j] = 'SPACE';
+        }
+    }
+
+    gridInfo[8][4] = 'EXIT';
+}
+
+void AddCars(std::vector<std::vector<int>> CHOSENBOARD)
+{
+    int i;
+
+    for(i = 0; i < size(CHOSENBOARD); i++) {
+
+        int j;
+        int colour = CHOSENBOARD[i][0];
+        int orientation = CHOSENBOARD[i][1];
+        int size = CHOSENBOARD[i][2];
+        int row = CHOSENBOARD[i][3];
+        int col = CHOSENBOARD[i][4];
+
+        if (orientation == 1) {
+		    for (j = col; j <= (col + size - 1 ); j++) {
+
+			    gridInfo[row][j] = colour;
+		    }
+        } else {
+            for (j = row; j <= (row + - 1 * size - 1); j++) {
+
+			    gridInfo[j][col] = colour;
+		    }
+        }
+	}
+
 }
 
 void PrintBoard()
@@ -117,42 +148,17 @@ void PrintBoard()
                 case 11:
                     colours[i][j] = createRGB(153, 255, 255);
                     break;
+                case 12:
+                    colours[i][j] = createRGB(0, 153, 153);
+                    break;
 
             }
         }
     }
 }
 
-void AddCars()
-{
-    int i;
-
-    for(i = 0; i < 2; i++) {
-
-        int j;
-        int colour = EASY1[i][0];
-        int orientation = EASY1[i][1];
-        int size = EASY1[i][2];
-        int row = EASY1[i][3];
-        int col = EASY1[i][4];
-
-        if (orientation == 1) {
-		    for (j = col; j <= (col + size - 1 ); j++) {
-
-			    gridInfo[row][j] = colour;
-		    }
-        } else {
-            for (j = row; j <= (row + - 1 * size - 1); j++) {
-
-			    gridInfo[j][col] = colour;
-		    }
-        }
-	}
-
-}
-
 void Rushhour()
 {
-    AddCars();
+    AddCars(EASY1);
     PrintBoard();
 }
